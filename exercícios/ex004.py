093e946d6916667701185245e07094cd2a332827
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-02-09 15:29:02 -0300

    exercício

--- exercícios/ex004.py
+++ exercícios/ex004.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
-soma = (n1 + n2)
-print('O valor da soma é de: {}'.format(soma))
+s = (n1 + n2)
+print('A soma entre {} e {} vale: {}'.format(n1, n2, s))
