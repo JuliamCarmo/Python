6532b1c4d392cb0025aeb7b50a637b1683cda2db
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-02-09 15:14:02 -0300

    exercicios

--- exercícios de teste/ex04.py
+++
@@ -1,4 +0,0 @@
-n1 = input('Digite um número: ')
-n2 = input('Digite outro número: ')
-soma = (n1 + n2)
-print('A soma entre', n1, '+', n2, 'é de:', soma)
---
+++ exercícios/ex04.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = (n1 + n2)
+print('O valor da soma é de: {}'.format(soma))
