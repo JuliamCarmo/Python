b1968a672e753df6b818891f450d2a1828c0a3a8
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-02-11 13:57:35 -0300

    exercício

--- exercícios/ex010.py
+++ exercícios/ex010.py
@@ -1,8 +1,6 @@
-medida = float(input('Digite  o valor de uma medida em METROS: '))
-km = medida / 1000
-hm = medida / 100
-dam = medida / 10
-dm = medida * 10
-cm = medida * 100
-mm = medida * 1000
-print('A medida de {} metros, corresponde a: \n{}km \n{}hm \n{}dam \n{:.0f}dm \n{:.0f}cm \n{:.0f}mm.'.format(medida, km, hm, dam, dm, cm, mm))
+med = float(input('Digite  o valor de uma medida em METROS: '))
+
+print('A medida de {} metros, corresponde a:'.format(med))
+print('\n{}km \n{}hm \n{}dam \n{:.0f}dm \n{:.0f}cm \n{:.0f}mm.'.format(med/1000, med/100, med/10, med*10, med*100, med*1000))
+
+#km = med / 1000, hm = med / 100, dam = med / 10, dm = med * 10, cm = med * 100, mm = med * 1000
