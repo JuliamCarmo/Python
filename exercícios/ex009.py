3bedea5220f3561330ebc5d2c132676f9952f4d7
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-02-11 12:44:32 -0300

    exercício

---
+++ exercícios/ex009.py
@@ -0,0 +1,5 @@
+nome = input('Digite o nome do aluno(a): ')
+n1 = float(input('Digite a primeira nota do aluno(a): '))
+n2 = float(input('Digite a segunda nota do aluno(a): '))
+m = (n1 + n2) / 2
+print('A média do aluno(a) {} é de: {:.1f}'.format(nome, m))
