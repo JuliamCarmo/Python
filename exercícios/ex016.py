db5609c511eb3fcadc0dec8d41ebfdfebbe9e58e
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-02-15 18:24:28 -0300

    exercício

--- exercícios/ex016.py
+++ exercícios/ex016.py
@@ -1,3 +1,3 @@
-c = float(input('Informa a temperatura em °C: '))
+c = float(input('Informe a temperatura em °C: '))
 f = ((9 * c) / 5) + 32
 print('A temperatura de {:.1f}°C corresponde a {:.1f}°F.'.format(c, f))
