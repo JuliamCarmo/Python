1de53447dcbf61ea360cb74613598478dd2cac31
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-02-16 13:10:28 -0300

    exercício

---
+++ exercícios/ex018.py
@@ -0,0 +1,7 @@
+from math import trunc
+
+num = float(input('Digite um valor: '))
+print('O valor {} tem sua porção inteira de {}.'.format(num, trunc(num)))
+
+#num = float(input('Digite um valor: '))
+#print('O valor {} tem sua porção inteira de {}.'.format(num, int(num)))
