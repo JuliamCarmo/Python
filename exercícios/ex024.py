690b5d548d36c22e133ea044ef1eb0b0d72a9724
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-02-22 15:53:09 -0300

    exercício

---
+++ exercícios/ex024.py
@@ -0,0 +1,8 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+print('Analisando seu nome...')
+print('Seu nome em letras maiúsculas é: {}'.format(nome.upper()))
+print('Seu nome em letras minúsculas é: {}'.format(nome.lower()))
+print('Seu nome tem {} letras'.format(len(nome) -   nome.count(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras.'.format(separa[0], len(separa[0])))
+#print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
